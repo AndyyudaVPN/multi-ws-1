d333d1b062cce6a4dc43e2658b13c08311d14479
AndyyudaVPN <134195192+AndyyudaVPN@users.noreply.github.com>
2023-06-15 20:05:50 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -256,7 +256,7 @@ echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
 # banner /etc/issue.net
 sleep 1
 echo -e "[ ${green}INFO$NC ] Settings banner"
-wget -q -O /etc/issue.net "https://raw.githubusercontent.com/Vlukss/multi-ws/main/issue.net"
+wget -q -O /etc/issue.net "https://raw.githubusercontent.com/AndyyudaVPN/multi-ws-1/main/issue.net"
 chmod +x /etc/issue.net
 echo "Banner /etc/issue.net" >> /etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
