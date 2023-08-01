bff6bc6923e2269c1609a87e0bfde493f887df07
AndyyudaVPN <134195192+AndyyudaVPN@users.noreply.github.com>
2023-08-02 02:15:17 +0700

    Update nontls.sh

--- websocket/nontls.sh
+++ websocket/nontls.sh
@@ -15,7 +15,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
 NC='\e[0m'
-echo "Installing SSH Websocket None TLS by Arh-Project" | lolcat
+echo "Installing SSH Websocket None TLS by AndyYuda" | lolcat
 echo "Progress..." | lolcat
 sleep 3
 
